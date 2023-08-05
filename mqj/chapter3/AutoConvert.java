ed560eb0d873996da09e3d7941baf831ac6b08a0
DESKTOP-FTIAOI5\maoqi <maoqijian1221@gmail.com>
2023-08-05 16:46:05 +0900

    chapter3

---
+++ mqj/chapter3/AutoConvert.java
@@ -0,0 +1,17 @@
+package com.mqj.chapter3;
+
+public class AutoConvert {
+	
+	// 编写一个main方法
+	public static void main(String[] args) {
+
+		// 演示自动转换
+
+		int num = 'a';// ok char -> int
+		double d1 = 80; // ok int -> double
+		System.out.println(num);// 97
+		System.out.println(d1);// 80.0
+
+	}
+
+}
---
+++ mqj/chapter3/StringToBasicDetail.java
@@ -0,0 +1,14 @@
+package com.mqj.chapter3;
+
+public class StringToBasicDetail {
+	
+	//编写一个main方法
+	public static void main(String[] args) {
+		
+		String str = "hello";
+		//转成int
+		int n1 = Integer.parseInt(str);
+		System.out.println(n1);
+	}
+
+}
