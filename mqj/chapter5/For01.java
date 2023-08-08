6bd36f12064eada69440ecc67f0f45b1eb9a4c60
DESKTOP-FTIAOI5\maoqi <maoqijian1221@gmail.com>
2023-08-08 16:53:22 +0900

    chapter5

---
+++ mqj/chapter5/For01.java
@@ -0,0 +1,27 @@
+package com.mqj.chapter5;
+
+public class For01 {
+	
+	//编写一个main方法
+	public static void main(String[] args) {
+		//打印10句 "你好，哈佛教育!"
+		//传统的方法
+		// System.out.println("你好，哈佛教育");
+		// System.out.println("你好，哈佛教育");
+		// System.out.println("你好，哈佛教育");
+		// System.out.println("你好，哈佛教育");
+		// System.out.println("你好，哈佛教育");
+		// System.out.println("你好，哈佛教育");
+		// System.out.println("你好，哈佛教育");
+		// System.out.println("你好，哈佛教育");
+		// System.out.println("你好，哈佛教育");
+		// System.out.println("你好，哈佛教育");
+		// 
+		// 使用for循环控制
+		for( int i = 1; i <= 10; i++) {
+			System.out.println("你好，哈佛教育" + i);
+		}
+		
+	}
+
+}
